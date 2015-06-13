451e188b938dee6615e862620cd79c4937cdd8a6
Jon Seymour <jon.seymour@gmail.com>
2015-06-13 16:18:33 +1000

    Add tests of APIManagerImpl.
    Signed-off-by: Jon Seymour <jon.seymour@gmail.com>

--- src/test/java/com/wildducktheories/api/impl/APIManagerTest.java
+++ src/test/java/com/wildducktheories/api/impl/APIManagerTest.java
@@ -1,6 +1,10 @@
 package com.wildducktheories.api.impl;
 
+import java.util.concurrent.Callable;
+
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import com.wildducktheories.api.API;
@@ -26,13 +30,26 @@ public class APIManagerTest {
 		};
 	}
 
+	private TestAPIManager manager;
+
+	@Before
+	public void setup()
+	{
+		manager = new TestAPIManager();
+	}
+
+	@After
+	public void after()
+	{
+		manager.reset();
+	}
+
 	/**
 	 * Test that resets causes a different object to be allocated. That the same
 	 * object will be returned without a reset and that the reset method is called on reset.
 	 */
 	@Test
 	public void testReset() {
-		final  TestAPIManager manager = new TestAPIManager();
 
 		TestAPI api1 = manager.get();
 		TestAPI api2 = manager.get();
@@ -47,4 +64,109 @@ public class APIManagerTest {
 		Assert.assertTrue(api1.reset);
 		Assert.assertFalse(api3.reset);
 	}
+
+	/**
+	 * Test with(TestAPI, Callable)
+	 * @throws Exception
+	 */
+	@Test
+	public void testWithCallable() throws Exception {
+		final TestAPI[] apis = new TestAPI[] { null, null, manager.create(), manager.get() };
+		final Integer result = manager.with(apis[2], new Callable<Integer>() {
+			@Override
+			public Integer call() throws Exception {
+				apis[0] = manager.get();
+				return 3141529;
+			}
+		});
+		Assert.assertEquals("Result is passed through", 3141529, (int)result); // result passed through
+		apis[1] = manager.get();
+		manager.reset();
+		Assert.assertNotNull("API is not null in body", apis[0]);
+		Assert.assertNotNull("API is not null on return", apis[1]); // api is not null on return
+		Assert.assertSame("API is restored on return", apis[1], apis[3]);
+		Assert.assertNotSame("API is different inside and outside", apis[0], apis[1]); // api
+		Assert.assertSame("API is same before and after", apis[0], apis[2]);
+	}
+
+	/**
+	 * Test with(TestAPI, Callable)
+	 */
+	@Test
+	public void testWithCallableThrows() {
+		final Exception thrown = new Exception();
+		final TestAPI[] apis = new TestAPI[] { null, null, manager.create(), manager.get() };
+		try {
+			manager.with(apis[2], new Callable<Integer>() {
+				@Override
+				public Integer call() throws Exception {
+					apis[0] = manager.get();
+					throw thrown;
+				}
+			});
+			Assert.fail("Failed to propagate exception");
+		} catch (Exception e) {
+			Assert.assertSame("Exception propagated", thrown, e);
+			apis[1] = manager.get();
+			manager.reset();
+			Assert.assertNotNull("API is not null in body", apis[0]);
+			Assert.assertNotNull("API is not null on return", apis[1]); // api is not null on return
+			Assert.assertSame("API is restored on return", apis[1], apis[3]);
+			Assert.assertNotSame("API is different inside and outside", apis[0], apis[1]); // api
+			Assert.assertSame("API is same before and after", apis[0], apis[2]);
+		}
+	}
+
+
+
+	/**
+	 * Test with(TestAPI, Runnable)
+	 */
+	@Test
+	public void testWithRunnable() {
+		final TestAPI[] apis = new TestAPI[] { null, null, manager.create(), manager.get() };
+		 manager.with(apis[2], new Runnable() {
+			@Override
+			public void run() {
+				apis[0] = manager.get();
+			}
+		});
+		apis[1] = manager.get();
+		manager.reset();
+		Assert.assertNotNull("API is not null in body", apis[0]);
+		Assert.assertNotNull("API is not null on return", apis[1]); // api is not null on return
+		Assert.assertSame("API is restored on return", apis[1], apis[3]);
+		Assert.assertNotSame("API is different inside and outside", apis[0], apis[1]); // api
+		Assert.assertSame("API is same before and after", apis[0], apis[2]);
+	}
+
+	/**
+	 * Test with(TestAPI, Runnable)
+	 */
+	@Test
+	public void testWithRunnableThrows() {
+		final RuntimeException thrown = new RuntimeException();
+
+		final TestAPI[] apis = new TestAPI[] { null, null, manager.create(), manager.get() };
+		try {
+			 manager.with(apis[2], new Runnable() {
+				@Override
+				public void run() {
+					apis[0] = manager.get();
+					throw thrown;
+				}
+			});
+			 Assert.fail("expected RuntimeException to be thrown");
+		} catch (RuntimeException e) {
+			Assert.assertSame(thrown, e);
+			apis[1] = manager.get();
+			manager.reset();
+			Assert.assertNotNull("API is not null in body", apis[0]);
+			Assert.assertNotNull("API is not null on return", apis[1]); // api is not null on return
+			Assert.assertSame("API is restored on return", apis[1], apis[3]);
+			Assert.assertNotSame("API is different inside and outside", apis[0], apis[1]); // api
+			Assert.assertSame("API is same before and after", apis[0], apis[2]);
+		}
+	}
+
 }
